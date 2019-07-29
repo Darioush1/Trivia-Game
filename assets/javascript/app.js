5b957e6e15948c6ba8062530ba288692ed7d2a55
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-07-28 22:18:12 -0700

    answers are in but not the other stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,15 @@
 var correct = true;
 var incorrect = false;
 var gameStartButton = '<p class="answers" id="startGamePrompt"> Yo!!! you think ready for this???</p>';
+var answer = '<p class="answers"></p>';
+var thisQuestion=0;
+
+var questionsQA = [{
+
+    question: "Who was the first president?",
+    answer: ["Dragon", "Dragon 2", "President George Washington", "President George Washingtons Dad"],
+    correctAnswer: "President George Washington",
+}]
 
 // <br><br> <button id="gameStartButton">Yo Lets Do This!</button> 
 
@@ -8,8 +17,17 @@ var gameStartButton = '<p class="answers" id="startGamePrompt"> Yo!!! you think 
   $('.answers').append('<br><br> <button class="gameStartButton" id="startGamePrompt">Yo Lets Do This!</button>') ;
 
 function generateAnswersSquare ( ) {
-    for (var i = 0; i < 4 ; i++) {
-        $('#question-box').append('<p class="answers"> </p>');
+
+    for (var i = 0; i < questionsQA[this.thisQuestion].answer.length ; i++) {
+
+        $('#question-box').append(
+            '<p class="answers"> <span class="clickAble"></span></p>'
+            );
+        $('.clickAble').html(  +  questionsQA[this.thisQuestion].answer[i] + );
+        $('.question-text').html(questionsQA[this.thisQuestion].question);
+        
+        console.log('this is the thing ' +  questionsQA[this.thisQuestion].answer[i] + ' ' + i);
+
     }
 };
      
@@ -34,7 +52,11 @@ $('.gameStartButton').on( 'click',  function ( ) {
 
 function assignAnswer ( ) {
     $('.answers').append()
-}
+};
+
+//  "<button class='mainbutton' id='button' callquestion=' " + trivqs[this.currentQ].answers[i] + " '>
+// " + trivqs[this.currentQ].answers[i] + "
+// </button>"
 
 
 
