493bbe50f32fa7af291edd954547ac907990de63
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-07-28 20:16:50 -0700

    made a fancy for loop to create answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,40 @@
 var correct = true;
 var incorrect = false;
 var gameStartButton = '<p class="answers" id="startGamePrompt"> Yo!!! you think ready for this???</p>';
+
 // <br><br> <button id="gameStartButton">Yo Lets Do This!</button> 
 
   $('#question-box').append(gameStartButton); 
-  $('.answers').append('<br><br> <button class="gameStartButton" id="startGamePrompt">Yo Lets Do This!</button>')   
+  $('.answers').append('<br><br> <button class="gameStartButton" id="startGamePrompt">Yo Lets Do This!</button>') ;
+
+function generateAnswersSquare ( ) {
+    for (var i = 0; i < 4 ; i++) {
+        $('#question-box').append('<p class="answers"> </p>');
+    }
+};
      
   function nextQuestion ( ) {
     
     var randomAnswer = Math.floor(Math.random() * 4) + 1;
     var correctAnswer;
     var wrongAnswer;
+    generateAnswersSquare();
 
-    $('#question-box').append('<p class="answers"> </p>');
-    $('#question-box').append('<p class="answers"> </p>');
-    $('#question-box').append('<p class="answers"> </p>');
-    $('#question-box').append('<p class="answers"> </p>');
+    // $('#question-box').append('<p class="answers"> </p>');
+    // $('#question-box').append('<p class="answers"> </p>');
+    // $('#question-box').append('<p class="answers"> </p>');
+    // $('#question-box').append('<p class="answers"> </p>');
 };
 
 $('.gameStartButton').on( 'click',  function ( ) {
-
     nextQuestion ( );
     $('#startGamePrompt').remove();
-
 })
 
+
+function assignAnswer ( ) {
+    $('.answers').append()
+}
 
 
 
