8195effa48fbff7cab0b65e9bf2e95104983225e
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-08-03 14:23:50 -0700

    player selction updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,61 +2,67 @@ var correct = true;
 var incorrect = false;
 // var answer = '<p class="answers"></p>';
 var thisQuestion = 0;
+var playerChoice;
+
 
 var questionsQA = [
     {
         question: "Who was the first president?",
         answer: ["Dragon", "Dragon 2", "President George Washington", "President George Washingtons Dad"],
         correctAnswer: "President George Washington",
+    },
+
+    {
+        question: "Who was the second president?",
+        answer: ["John Adams", "Bill Clinton", "Aohn Jdmas", "President George Washingtons other Dad"],
+        correctAnswer: "John Adams",
     }
 ]
-// questionsQA[0].answer[i]
 
 $('.container').ready(function () {
     $('#questionBox').hide();
 })
 
 
-$('.answers').on('click', function () {
-    let playerChoice = $(this).attr('question-text');
-    console.log(playerChoice)
-});
 
-
-
-function nextQuestion() {
-
-    var randomAnswer = Math.floor(Math.random() * 4) + 1;
-    var correctAnswerDom = questionsQA[this.thisQuestion].correctAnswer;
-    var wrongAnswer;
-    // generateAnswersSquare();
-  //  generateAnswersText();
-
-
-};
-
-$('.gameStartButton').on('click', function () {
-    nextQuestion();
-    $('#question-box1').remove();
-    $('#questionBox').show();
-    generateAnswersText()
-
-})
 
 function generateAnswersText() {
 
-    for (var i = 0; i < questionsQA[i].length; i++) {
-        for(var j = 0;  j< questionsQA[i].answer.length; j++){
-        $('.answers1').html(questionsQA[i].answer[j]);
-        // $('#question-box').append('<p class="answers" id=" ' + questionsQA[this.thisQuestion].answer[i] + ' "> </p>' );
-        //$('#' + questionsQA[this.thisQuestion].answer[i] + '').innerHTML(" ' + questionsQA[this.thisQuestion].answer[i] + ' ")
-      //  $('.question-text').innerHTML(questionsQA[this.thisQuestion].question);
+    for (let i = 0; i < questionsQA.length; i++) {
 
-        console.log('this is the thing ' + questionsQA[i].answer[j]);
+        $('#question-text').text(questionsQA[this.thisQuestion].question);
+        for (let j = 0; j < questionsQA[this.thisQuestion].answer.length; j++) {
+
+            console.log(questionsQA[this.thisQuestion].answer[j]);
+
+            $('.answers' + j).text(questionsQA[this.thisQuestion].answer[j]);
         }
     }
 };
 
 
+
+function playerGuess() {
+    if (playerChoice == questionsQA[this.questionsQA].correctAnswer) {
+        console.log("correct")
+    } else if (playerChoice !== questionsQA[this.questionsQA].correctAnswer) {
+        console.log('naw')
+    }
+};
+
+$('.gameStartButton').on('click', function () {
+
+    $('#question-box1').remove();
+    $('#questionBox').show();
+    generateAnswersText();
+
+});
+
+$('.answers').on('click', function () {
+    let playerChoice = $(this).text();
+    console.log("Player Choice: " + playerChoice);
+    playerGuess();
+    console.log(questionsQA.correctAnswer);
+});
 
 
