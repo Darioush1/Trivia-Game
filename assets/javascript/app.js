5e58c6af323cb0a4b8608a128bb66f047b14de2c
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-08-15 00:58:29 -0700

    timer added just have to stop it

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ var correct = true;
 var incorrect = false;
 // var answer = '<p class="answers"></p>';
 var thisQuestion = 0;
-var playerChoice = '';
+var playerChoice = ' ';
 var timer;
 var sec = 10;
 var points = 0;
@@ -23,8 +23,6 @@ var questionsQA = [
 
 $('.container').ready(function () {
     $('#questionBox').hide();
-    console.log(sec);
-    console.log(points)
 })
 
 
@@ -37,7 +35,7 @@ function generateAnswersText() {
         $('#question-text').text(questionsQA[this.thisQuestion].question);
 
         for (let j = 0; j < questionsQA[this.thisQuestion].answer.length; j++) {
-            console.log(questionsQA[this.thisQuestion].answer[j]);
+          console.log(questionsQA[this.thisQuestion].correctAnswer);
             $('.answers' + j).text(questionsQA[this.thisQuestion].answer[j]);
         }
     }
@@ -46,7 +44,7 @@ function generateAnswersText() {
 function timeOut() {
     $('#clock').text('you lost');
     if (sec === 0) {
-        console.log("HAPPY NEW YEAR!!");
+        console.log('timeOut function hit zero');
     }
 };
 
@@ -54,22 +52,40 @@ function nextQuestion() {
     thisQuestion++;
     generateAnswersText();
     setInterval(timeOut, 10000);
+    setInterval(countdown, 1000);
 
-}
+};
+
+function countdown() {
+    sec--;
+    if(sec===0) {
+        clearInterval(sec)
+    };
+    console.log('time left: ' + sec)
+};
 
 function playerGuess() {
    let  correctChoice = questionsQA[this.thisQuestion].correctAnswer;
-    if (playerChoice === correctChoice) {
-       
+  console.log("playerGuess Function " + playerChoice);
+  console.log("playerGuess Function answer " + correctChoice);
+    if (playerChoice == correctChoice) {
         console.log("function: " + playerChoice);
-        nextQuestion()
-        console.log()
+        inbetweenQuestionsCorrect();
+        points++;
+        console.log("in function " +points)
 
     } else {
        
         console.log('correct answer: ' + questionsQA[this.thisQuestion].correctAnswer);
-        nextQuestion();
+       inbetweenQuestionsWrong();
     }
+};
+
+function inbetweenQuestionsCorrect ( ){
+     $(".question-text").text('you did not mess up!')
+};
+function inbetweenQuestionsWrong ( ){
+    $(".question-text").text('you did mess up!!! Mom hates you now!!')
 };
 
 $('.gameStartButton').on('click', function () {
@@ -80,11 +96,13 @@ $('.gameStartButton').on('click', function () {
     $('#timerSpot').html(sec);
     generateAnswersText();
     setInterval(timeOut, 10000);
+    setInterval(countdown, 1000);
+
 
 });
 
 $('.answers').on('click', function () {
-    let playerChoice =  $(this).html();
+    playerChoice =  $(this).html();
     console.log("Player Choice: " + playerChoice);
     playerGuess();
     console.log('this questions number: ' + thisQuestion);
