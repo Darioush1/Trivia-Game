7ca451b5d4350542a8d4552a47aa93cc1db84be1
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-08-16 01:21:55 -0700

    timer being funky

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,11 @@
 var correct = true;
 var incorrect = false;
-// var answer = '<p class="answers"></p>';
 var thisQuestion = 0;
 var playerChoice = ' ';
 var timer;
 var sec = 10;
 var points = 0;
+var timerSpot = document.getElementById('timerSpot');
 
 var questionsQA = [
     {
@@ -24,7 +24,7 @@ var questionsQA = [
         answer: ["John Adams", "Bill Clinton", "Aohn Jdmas", "President George Washingtons other Dad"],
         correctAnswer: ["John Adams"],
     },
-    
+
     {
         question: "space filler",
         answer: ["space filler"],
@@ -41,7 +41,8 @@ var questionsQA = [
 
 $('.container').ready(function () {
     $('#questionBox').hide();
-})
+    $('#resultBox').hide()
+});
 
 
 
@@ -61,10 +62,23 @@ function generateAnswersText() {
 //function to stop the clock at zero
 function timeOut() {
     $('#clock').text('you lost');
-    if (sec === 0) {
-        clearInterval(timeOut)
-        clearInterval(countdown)
-    }
+    console.log(sec);
+    inbetweenQuestionsNoTime();
+};
+
+//adjust Dom timer
+function domTimer() {
+    sec--;
+    timerSpot--;
+    console.log(sec)
+};
+
+var timeOutObj = setInterval(timeOut, 10000);
+var domTimerObj = setInterval(domTimer, 1000);
+
+function stopTimers( ) {
+    clearInterval(timeOutObj);
+    clearInterval(domTimerObj);
 };
 
 //funtion to move on to next question in array and reset timer
@@ -73,49 +87,63 @@ function nextQuestion() {
     $('.answers').show();
     thisQuestion++;
     generateAnswersText();
-    setInterval(timeOut, 10000);
-    setInterval(countdown, 1000);
-};
-
-//function to create timer
-function countdown() {
-    sec--;
-    if(sec==0) {
-        clearInterval(timeOut);
-        clearInterval(countdown);
-    };
-  //  console.log('time left: ' + sec)
+   
 };
 
 //function to create timer that will hide answers and present a new slide inbetween the answers
-function inbetweenQuestionsCorrect ( ){
+function inbetweenQuestionsCorrect() {
     $("#question-text").text('you did not mess up!');
     $('.answers').hide();
     $('.nextQuestionButton').show();
-    clearInterval(timeOut);
-    clearInterval(countdown);
+    
 };
 
 //function to create a new slide that will show when the answer is wrong
-function inbetweenQuestionsWrong ( ){
-   $("#question-text").text('you did mess up!!! Mom hates you now!!');
-   $('.answers').hide();
-   $('.nextQuestionButton').show();
-   clearInterval(timeOut);
-   clearInterval(countdown);
+function inbetweenQuestionsWrong() {
+    $("#question-text").text('you did mess up!!! Mom hates you now!!');
+    $('.answers').hide();
+    $('.nextQuestionButton').show();
 };
 
+function inbetweenQuestionsNoTime() {
+    $("#question-text").text('Just like that time in middle school when you didn\'t ask out Torry K. you hesistated. You loooose this round!');
+    $('.answers').hide();
+    $('.nextQuestionButton').show();
+};
+
+
+//function that creates the players choice and holds the events depending on the results
 function playerGuess() {
-   let  correctChoice = questionsQA[this.thisQuestion].correctAnswer;
-  console.log("playerGuess Function " + playerChoice);
-  console.log("playerGuess Function answer " + correctChoice);
-    if (playerChoice == correctChoice) {
+
+    let correctChoice = questionsQA[this.thisQuestion].correctAnswer;
+
+    if (thisQuestion === 6) {
+        showResults();
+        $('#questionBox').hide();
+    }  else if (playerChoice == correctChoice) {
         inbetweenQuestionsCorrect();
         points++;
-        
+        console.log(points)
+        stopTimers();
+    } else if (sec === 0 ){
+        stopTimers();
     } else {
         console.log('correct answer: ' + questionsQA[this.thisQuestion].correctAnswer);
-       inbetweenQuestionsWrong();
+        inbetweenQuestionsWrong();
+        console.log(points);
+    }
+};
+
+
+//shows a message at the end of the game
+function showResults() {
+    $('#resultBox').show();
+    if ( points === 3) {
+        $('.resultMessage').text("You did it! The President has been saved from ninjas! Now Git!")
+    } else if ( points === 2) {
+        $('.resultMessage').text('You partially did it! You saved half the president! The good half!')
+    } else {
+        $('.resultMessage').text('The Ninjas have won, the country its doomed. Farewell friend')
     }
 };
 
@@ -123,26 +151,37 @@ function playerGuess() {
 //start the game on click
 $('.gameStartButton').on('click', function () {
 
-    //let timer = setInterval(tim)
     $('#question-box1').remove();
     $('#questionBox').show();
     $('#timerSpot').html(sec);
     generateAnswersText();
-    setInterval(timeOut, 10000);
-    setInterval(countdown, 1000);
+    timeOutObj;
+    domTimerObj;
     $('.nextQuestionButton').hide();
 });
 
 
 //player selects an answer
 $('.answers').on('click', function () {
-    playerChoice =  $(this).html();
+    playerChoice = $(this).html();
     playerGuess();
     thisQuestion++;
+    console.log('Question array number: ' + thisQuestion)
 });
 
+//this will eiher present the result page or the next question
 $('.nextQuestionButton').on('click', function () {
+    
+    console.log('Question array number: ' + thisQuestion)
+    if (thisQuestion == 7) {
+        showResults();
+        $('#questionBox').hide();
+    } else {
     nextQuestion();
+    timeOutObj;
+    domTimerObj;
+    console.log(sec);
+    }
 });
 
 
