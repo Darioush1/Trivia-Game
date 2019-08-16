85ad7d21de2944364bca2dd5c4129b59d97a25d0
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-08-15 22:44:06 -0700

    Inbetween page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,11 +13,29 @@ var questionsQA = [
         answer: ["Dragon", "Dragon 2", "President George Washington", "President George Washingtons Dad"],
         correctAnswer: "President George Washington",
     },
+    {
+        question: "space filler",
+        answer: ["space filler"],
+        correctAnswer: "space filler",
+    },
 
     {
         question: "Who was the second president?",
         answer: ["John Adams", "Bill Clinton", "Aohn Jdmas", "President George Washingtons other Dad"],
         correctAnswer: ["John Adams"],
+    },
+    
+    {
+        question: "space filler",
+        answer: ["space filler"],
+        correctAnswer: "space filler",
+    },
+
+
+    {
+        question: "Who was the best president?",
+        answer: ["President Comacho", "Abraham Lincoln", "Bill Paxton", "The Secret Elite"],
+        correctAnswer: ["Abraham Lincoln"],
     }
 ];
 
@@ -27,7 +45,7 @@ $('.container').ready(function () {
 
 
 
-
+//funtion to make answers append to the page
 function generateAnswersText() {
 
     for (let i = 0; i < questionsQA.length; i++) {
@@ -35,45 +53,54 @@ function generateAnswersText() {
         $('#question-text').text(questionsQA[this.thisQuestion].question);
 
         for (let j = 0; j < questionsQA[this.thisQuestion].answer.length; j++) {
-         // console.log(questionsQA[this.thisQuestion].correctAnswer);
             $('.answers' + j).text(questionsQA[this.thisQuestion].answer[j]);
         }
     }
 };
 
+//function to stop the clock at zero
 function timeOut() {
     $('#clock').text('you lost');
     if (sec === 0) {
-        console.log('timeOut function hit zero');
         clearInterval(timeOut)
+        clearInterval(countdown)
     }
 };
 
+//funtion to move on to next question in array and reset timer
 function nextQuestion() {
+    $('.nextQuestionButton').hide();
+    $('.answers').show();
     thisQuestion++;
     generateAnswersText();
     setInterval(timeOut, 10000);
     setInterval(countdown, 1000);
 };
 
+//function to create timer
 function countdown() {
     sec--;
     if(sec==0) {
         clearInterval(timeOut);
         clearInterval(countdown);
     };
-    console.log('time left: ' + sec)
+  //  console.log('time left: ' + sec)
 };
 
+//function to create timer that will hide answers and present a new slide inbetween the answers
 function inbetweenQuestionsCorrect ( ){
     $("#question-text").text('you did not mess up!');
     $('.answers').hide();
+    $('.nextQuestionButton').show();
     clearInterval(timeOut);
     clearInterval(countdown);
 };
+
+//function to create a new slide that will show when the answer is wrong
 function inbetweenQuestionsWrong ( ){
    $("#question-text").text('you did mess up!!! Mom hates you now!!');
-   $('.answers').hide()
+   $('.answers').hide();
+   $('.nextQuestionButton').show();
    clearInterval(timeOut);
    clearInterval(countdown);
 };
@@ -86,16 +113,14 @@ function playerGuess() {
         inbetweenQuestionsCorrect();
         points++;
         
-
     } else {
-       
         console.log('correct answer: ' + questionsQA[this.thisQuestion].correctAnswer);
        inbetweenQuestionsWrong();
     }
 };
 
 
-
+//start the game on click
 $('.gameStartButton').on('click', function () {
 
     //let timer = setInterval(tim)
@@ -105,12 +130,19 @@ $('.gameStartButton').on('click', function () {
     generateAnswersText();
     setInterval(timeOut, 10000);
     setInterval(countdown, 1000);
+    $('.nextQuestionButton').hide();
 });
 
+
+//player selects an answer
 $('.answers').on('click', function () {
     playerChoice =  $(this).html();
     playerGuess();
     thisQuestion++;
+});
+
+$('.nextQuestionButton').on('click', function () {
+    nextQuestion();
 });
 
 
